f92bdf9b0352c2c32f1415462c661d7cef7ace71
PrajwalVN <prajwalnagle1530@gmail.com>
2025-08-29 00:22:31 +0530

    deployment edit

--- backend/parking/settings.py
+++ backend/parking/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import pymongo
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,6 +57,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
